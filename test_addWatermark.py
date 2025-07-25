a68a9faee89ea3e1c23d35b003fe5825d1fc21ca
Fukuei-roc <f113097@yahoo.com.tw>
2025-07-26 01:04:39 +0800

    移除多餘程式

--- test_addWatermark.py
+++
@@ -1,6 +0,0 @@
-from addWatermark import add_watermark
-
-input_path = "pinkNight_upscaled_00028_resized.jpg"
-output_path = "pinkNight_upscaled_00028_mark.jpg"
-
-add_watermark(input_path, output_path)
--- test_resizeAndConvertImage.py
+++
@@ -1,6 +0,0 @@
-from resizeAndConvertImage import resize_and_convert
-
-input_path = "pinkNight_upscaled_00028_.png"
-output_path = "pinkNight_upscaled_00028_resized.jpg"
-
-resize_and_convert(input_path, output_path)
